48fb0a3f6d7b5f58cbf67aca348bf3cc96e1df30
ziegler <ziegler@edu-sharing.net>
2024-01-17 11:30:35 +0100

    Init

---
+++ src/main/kotlin/org/edu_sharing/rendering/ServicesRenderingService2Application.kt
@@ -0,0 +1,11 @@
+package org.edu_sharing.rendering
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServicesRenderingService2Application
+
+fun main(args: Array<String>) {
+    runApplication<ServicesRenderingService2Application>(*args)
+}
