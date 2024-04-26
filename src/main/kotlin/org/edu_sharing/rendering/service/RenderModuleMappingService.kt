e953d99cf0313182af561af2c9ca854b5db60ebb
Sebastian Wollner <wollner@edu-sharing.net>
2024-04-26 10:08:20 +0200

    ref: simplify if statements

--- src/main/kotlin/org/edu_sharing/rendering/service/RenderModuleMappingService.kt
+++ src/main/kotlin/org/edu_sharing/rendering/service/RenderModuleMappingService.kt
@@ -6,29 +6,25 @@ import org.springframework.stereotype.Service
 
 @Service
 class RenderModuleMappingService {
-    fun getModule(type: String = "", mimeType: String = ""): RenderModules {
-        if (type == "moodle") {
-            return RenderModules.MOODLE
-        }
-        if (type == "scorm") {
-            return RenderModules.SCORM
-        }
-        if (type == "eduhtml") {
-            return RenderModules.EDUHTML
-        }
-        return when (mimeType.substringBefore("/")) {
-            "audio" -> RenderModules.AUDIO
-            "video" -> RenderModules.VIDEO
-            "application" -> mapApplication(mimeType)
-            "image" -> RenderModules.IMAGE
-            else -> RenderModules.DEFAULT
-        }
+    fun getModule(type: String = "", mimeType: String = "") = when (type) {
+        "moodle" -> RenderModules.MOODLE
+        "scorm" -> RenderModules.SCORM
+        "eduhtml" -> RenderModules.EDUHTML
+        else -> getByMimeType(mimeType)
+    }
+    
+
+    private fun getByMimeType(mimeType: String) = when (mimeType.substringBefore("/")) {
+        "audio" -> RenderModules.AUDIO
+        "video" -> RenderModules.VIDEO
+        "image" -> RenderModules.IMAGE
+        "application" -> mapApplication(mimeType)
+        else -> RenderModules.DEFAULT
     }
 
-    private fun mapApplication(mimeType: String): RenderModules {
-        return when (mimeType) {
-            MediaType.APPLICATION_PDF_VALUE -> RenderModules.PDF
-            else -> RenderModules.DEFAULT
-        }
+    private fun mapApplication(mimeType: String) = when (mimeType) {
+        MediaType.APPLICATION_PDF_VALUE -> RenderModules.PDF
+        else -> RenderModules.DEFAULT
     }
+
 }
