003844a30789c79a2b419c1b5dbe461230bc8bea
Sebastian Wollner <wollner@edu-sharing.net>
2024-10-09 10:59:17 +0200

    fix: Mongodb WriteConcernResolver not used

--- service/src/main/kotlin/org/edu_sharing/rendering/config/MongoConfig.kt
+++ service/src/main/kotlin/org/edu_sharing/rendering/config/MongoConfig.kt
@@ -7,8 +7,10 @@ import org.edu_sharing.rendering.renderingJob.entity.SubJob
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.auditing.DateTimeProvider
+import org.springframework.data.mongodb.MongoDatabaseFactory
 import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.core.MongoAction
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.WriteConcernResolver
 import java.time.OffsetDateTime
 import java.util.*
@@ -17,7 +19,6 @@ import java.util.*
 @Configuration
 class MongoConfig {
 
-    @Bean
     fun writeConcernResolver(): WriteConcernResolver {
         return WriteConcernResolver { action: MongoAction? ->
             when {
@@ -28,6 +29,13 @@ class MongoConfig {
                 else -> WriteConcern.UNACKNOWLEDGED
             }
         }
+    }
+
+    @Bean
+    fun mongoTemplate(mongoDatabaseFactory: MongoDatabaseFactory): MongoTemplate {
+        val mongoTemplate = MongoTemplate(mongoDatabaseFactory)
+        mongoTemplate.setWriteConcernResolver(writeConcernResolver())
+        return mongoTemplate
     }
 
     @Bean
