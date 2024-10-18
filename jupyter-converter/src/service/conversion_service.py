396a8de89afdf6c2a06a050b1846bb970b8f56af
ziegler <ziegler@edu-sharing.net>
2024-10-18 14:28:25 +0200

    feat: cleanup jupyter converter

--- jupyter-converter/src/service/conversion_service.py
+++
@@ -1,9 +0,0 @@
-import typing as t
-
-from fastapi import UploadFile
-from nbconvert import HTMLExporter
-
-
-def convert_ipynb_to_html(file: UploadFile) -> tuple[str, dict[str, t.Any]]:
-    html_exporter: HTMLExporter = HTMLExporter()
-    return html_exporter.from_file(file.file)
