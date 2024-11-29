01dec503ce44ba9f10f23b61b317638ea76e2d55
Sebastian Wollner <wollner@edu-sharing.net>
2024-11-29 10:41:19 +0100

    refactoring: use common mongodb deployment

--- deploy/docker/build/lumi/src/main/build/assets/entrypoint.sh
+++ deploy/docker/build/lumi/src/main/build/assets/entrypoint.sh
@@ -4,6 +4,10 @@ set -eu
 
 mongodb_host=${MONGODB_HOST:?Error: MONGODB_HOST environment variable is not set}
 mongodb_port=${MONGODB_PORT:?Error: MONGODB_PORT environment variable is not set}
+mongodb_username=${MONGODB_USER:-MONGODB_USER environment variable is not set}
+mongodb_password=${MONGODB_PASSWORD:-MONGODB_PASSWORD environment variable is not set}
+mongodb_authdb=${MONGODB_AUTH_DB:-admin}
+
 
 minio_host=${MINIO_HOST:?Error: MINIO_HOST environment variable is not set}
 minio_port=${MINIO_PORT:?Error: MINIO_PORT environment variable is not set}
@@ -20,6 +24,9 @@ until nc -z "${minio_host}" "${minio_port}"; do
   sleep 2
 done
 
+# we need to wait, because mongodb sometimes need some more time...
+sleep 2
+
 export AWS_S3_ENDPOINT="http://${minio_host}:${minio_port}"
-export MONGODB_URL="mongodb://${mongodb_host}:${mongodb_port}"
+export MONGODB_URL="mongodb://${mongodb_username}:${mongodb_password}@${mongodb_host}:${mongodb_port}/?authSource=${mongodb_authdb}"
 sh -c "$*"
