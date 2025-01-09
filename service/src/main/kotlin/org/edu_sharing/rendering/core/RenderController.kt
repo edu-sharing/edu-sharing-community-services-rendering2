2ce527e28a5d261c8808f6fcd7d3f177e48e1696
ziegler <ziegler@edu-sharing.net>
2025-01-09 07:59:05 +0100

    feat: add response header in render data response

--- service/src/main/kotlin/org/edu_sharing/rendering/core/RenderController.kt
+++ service/src/main/kotlin/org/edu_sharing/rendering/core/RenderController.kt
@@ -5,20 +5,30 @@ import jakarta.validation.Valid
 import org.edu_sharing.rendering.core.annotation.ConditionalOnController
 import org.edu_sharing.rendering.core.dto.RenderDataRequest
 import org.edu_sharing.rendering.core.dto.RenderDataResponse
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @ConditionalOnController
 @RequestMapping("/public/renderdata")
 class RenderController (private val service: RenderDataService){
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @SecurityRequirement(name = "bearerAuth")
     @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getRenderData(@RequestBody @Valid body: RenderDataRequest): RenderDataResponse {
-        return service.getRenderData(body)
+    fun getRenderData(
+        @RequestBody @Valid body: RenderDataRequest,
+        @RequestHeader headers: Map<String, String>
+    ): ResponseEntity<RenderDataResponse> {
+        log.info(headers["authorization"])
+        return ResponseEntity
+            .ok()
+            .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS,"ES_RENDER_SSID")
+            .header("ES_RENDER_SSID", "SES123123")
+            .body(service.getRenderData(body))
     }
 }
