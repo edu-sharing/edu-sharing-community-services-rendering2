c0f32d1c96d4d2c41ca5a20a1a54304f69ba5952
ziegler <ziegler@edu-sharing.net>
2025-01-29 16:08:19 +0100

    fix: always allow module access with disabled security

--- service/src/main/kotlin/org/edu_sharing/rendering/security/ModulePermissionService.kt
+++ service/src/main/kotlin/org/edu_sharing/rendering/security/ModulePermissionService.kt
@@ -1,16 +1,21 @@
 package org.edu_sharing.rendering.security
 
 import org.edu_sharing.rendering.edusharingRepo.services.RepositoryRegistrationStorageService
-import org.edu_sharing.rendering.modules.ModuleRegistry
 import org.edu_sharing.rendering.modules.RenderModule
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class ModulePermissionService (
     private val nodePermissionSessionContextRepository: NodePermissionSessionContextRepository,
     private val repositoryRegistrationStorageService: RepositoryRegistrationStorageService,
+    @Value("\${app.security.enabled}")
+    private val securityEnabled: Boolean
 ){
     fun hasModuleAccess(renderModule: RenderModule, nodeId:String): Boolean {
+        if (!securityEnabled) {
+            return true
+        }
         val nodePermission = nodePermissionSessionContextRepository.getNodePermission(nodeId)
         return nodePermission != null && hasModuleAccessInternal(renderModule, nodePermission.repoId)
     }
