2da6a0464dca1093fe473b8b2a240c353190cb96
ziegler <ziegler@edu-sharing.net>
2025-03-18 11:41:36 +0100

    feat: add ping endpoint to jupyter converter

--- jupyter-converter/main.py
+++ jupyter-converter/main.py
@@ -1,9 +1,13 @@
 import uvicorn
-from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi import FastAPI, UploadFile, Response
 from starlette.responses import HTMLResponse
 from nbconvert import HTMLExporter
 
 app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return Response(status_code=200)
 
 @app.post("/convert")
 async def convert(file: UploadFile):
