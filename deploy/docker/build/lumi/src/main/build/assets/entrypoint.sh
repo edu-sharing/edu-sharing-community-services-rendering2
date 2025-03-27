a4bbc5be95860bbf21ca8600560f655bd351e673
Sebastian Wollner <wollner@edu-sharing.net>
2025-03-27 11:47:52 +0100

    feat: lumi added MONGODB_URI for cluster config

--- deploy/docker/build/lumi/src/main/build/assets/entrypoint.sh
+++ deploy/docker/build/lumi/src/main/build/assets/entrypoint.sh
@@ -29,5 +29,5 @@ sleep 2
 
 export DEBUG="${LOG_SCOPE:-h5p:*}"
 export AWS_S3_ENDPOINT="http://${minio_host}:${minio_port}"
-export MONGODB_URL="mongodb://${mongodb_username}:${mongodb_password}@${mongodb_host}:${mongodb_port}/?authSource=${mongodb_authdb}"
+export MONGODB_URL="${MONGODB_URI:-"mongodb://${mongodb_username}:${mongodb_password}@${mongodb_host}:${mongodb_port}/?authSource=${mongodb_authdb}"}"
 sh -c "$*"
