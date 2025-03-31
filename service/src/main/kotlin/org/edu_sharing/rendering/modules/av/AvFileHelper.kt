6b46a888507e96b4455a103f6012af2cd8c0803b
Sebastian Wollner <wollner@edu-sharing.net>
2025-03-31 10:43:48 +0200

    fix: permission issues on docker

--- service/src/main/kotlin/org/edu_sharing/rendering/modules/av/AvFileHelper.kt
+++ service/src/main/kotlin/org/edu_sharing/rendering/modules/av/AvFileHelper.kt
@@ -2,28 +2,24 @@ package org.edu_sharing.rendering.modules.av
 
 import org.edu_sharing.rendering.core.dto.CacheObject
 import org.edu_sharing.rendering.storage.StorageService
+import org.springframework.beans.factory.annotation.Value
 import java.io.Closeable
 import java.io.File
 import java.nio.file.Files
 import java.util.*
 
 class AvFileHelper(
-    private val storageImplementation: StorageService
+    private val storageImplementation: StorageService,
 ): Closeable {
     lateinit var outputFile: File
     lateinit var originalFile: File
 
     fun initOutputTempFile(extension: String) {
-        outputFile = File("${UUID.randomUUID()}.$extension")
+        outputFile = File.createTempFile(UUID.randomUUID().toString(), extension)
     }
 
     fun fetchOriginalTempFile(cacheObject: CacheObject) {
-        val originalFileName = buildString {
-            append(UUID.randomUUID().toString())
-            append(".")
-            append(cacheObject.mimeType.substringAfter('/'))
-        }
-        originalFile = File(originalFileName)
+        originalFile = File.createTempFile(UUID.randomUUID().toString(), cacheObject.mimeType.substringAfter('/'))
         val fileInputStream = storageImplementation.getObjectStream(cacheObject, true)
         fileInputStream.use {
             Files.copy(fileInputStream, originalFile.toPath())
