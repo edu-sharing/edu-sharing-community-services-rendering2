47190d5ae062dd887342f9ad810cabd20d847b8f
Sebastian Wollner <wollner@edu-sharing.net>
2025-04-09 12:05:25 +0200

    fix: roles doesn't work anymore since springboot 3.4.4 update

--- service/src/main/kotlin/org/edu_sharing/rendering/modules/av/ConditionalOnAvConverter.kt
+++ service/src/main/kotlin/org/edu_sharing/rendering/modules/av/ConditionalOnAvConverter.kt
@@ -2,7 +2,7 @@ package org.edu_sharing.rendering.modules.av
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 
-@ConditionalOnExpression("T(org.springframework.util.StringUtils).isEmpty('\${app.roles:}') || T(java.util.Arrays).asList('\${app.roles:}').contains('avconverter')")
+@ConditionalOnExpression("T(org.springframework.util.StringUtils).isEmpty('\${app.roles:}') || T(java.util.Arrays).asList('\${app.roles:}'.split(',')).contains('avconverter')")
 @Target(allowedTargets = [AnnotationTarget.TYPE, AnnotationTarget.FUNCTION, AnnotationTarget.CLASS])
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ConditionalOnAvConverter ()
