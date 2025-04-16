eb3f7820d12411383a6388e33196f6043fbfc062
ziegler <ziegler@edu-sharing.net>
2025-04-16 15:08:02 +0200

    feat: error strings

--- service/src/main/kotlin/org/edu_sharing/rendering/core/ErrorStrings.kt
+++ service/src/main/kotlin/org/edu_sharing/rendering/core/ErrorStrings.kt
@@ -1,11 +1,11 @@
 package org.edu_sharing.rendering.core
 
 object ErrorStrings {
-    const val ERROR_PROCESSING_JOB = "error_processing_job"
-    const val GENERIC_CONVERSION_ERROR = "generic_version_error"
-    const val GENERIC_NOT_FOUND = "generic_not_found"
-    const val GENERIC_BAD_REQUEST = "generic_bad_request"
-    const val GENERIC_MODULE_NOT_AVAILABLE = "generic_module_not_available"
-    const val GENERIC_OBJECT_NOT_SUPPORTED = "generic_object_not_supported"
-    const val GENERIC_INTERNAL_SERVER_ERROR = "generic_internal_server_error"
+    const val ERROR_PROCESSING_JOB = "RENDERING.ERROR.ERROR_PROCESSING_JOB"
+    const val GENERIC_CONVERSION_ERROR = "RENDERING.ERROR.GENERIC_CONVERSION_ERROR"
+    const val GENERIC_NOT_FOUND = "RENDERING.ERROR.GENERIC_NOT_FOUND"
+    const val GENERIC_BAD_REQUEST = "RENDERING.ERROR.GENERIC_BAD_REQUEST"
+    const val GENERIC_MODULE_NOT_AVAILABLE = "RENDERING.ERROR.GENERIC_MODULE_NOT_AVAILABLE"
+    const val GENERIC_OBJECT_NOT_SUPPORTED = "RENDERING.ERROR.GENERIC_OBJECT_NOT_SUPPORTED"
+    const val GENERIC_INTERNAL_SERVER_ERROR = "RENDERING.ERROR.GENERIC_INTERNAL_SERVER_ERROR"
 }
